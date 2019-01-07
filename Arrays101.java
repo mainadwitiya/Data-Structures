7f57caa9917a893cf8efed8ec6cfe09c6f368963
mainadwitiya <adw.tri2012@gmail.com>
2019-01-07 05:59:14 +0530

    All OK

---
+++ Arrays101.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Arrays101 {
+    public static void main(String[] args) {
+
+        Scanner ss = new Scanner(System.in);
+
+
+        int [] intArray = new int[6];
+        for(int i = 0 ; i<intArray.length ; i++){
+            intArray[i] = ss.nextInt();
+        }
+       /* intArray[0] = 20;
+        intArray[1] = 50;
+        intArray[2] = 40;
+        intArray[3] = 30;
+        intArray[4] = 10;
+        intArray[5] = 70;
+        intArray[6] = 29;*/
+
+        for(int i = 0 ; i< intArray.length;i++)
+    {
+        System.out.println(intArray[i]);
+
+    }
+
+
+}
+}
