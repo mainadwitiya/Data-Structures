22a621e256f4a63975979db5831d8821a8396809
mainadwitiya <adw.tri2012@gmail.com>
2021-11-22 22:56:50 +0530

    leet_code_sol 35

---
+++ python_codes/leet_code/35_search_insert_position.py
@@ -0,0 +1,21 @@
+"""
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+"""
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if target in nums:
+            return nums.index(target)
+        else:
+            nums.append(target)
+            nums.sort()
+            return nums.index(target)
