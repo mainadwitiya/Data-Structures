ba3be51ed683fe439e6ad20d51de0818d76ea1f1
Adwitiya Trivedi <42941253+mainadwitiya@users.noreply.github.com>
2022-01-26 22:53:10 +0530

    easy leet
    easy leet

---
+++ python_codes/leet_code/20_Valid_Parentheses.py
@@ -0,0 +1,25 @@
+'''
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+'''
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        while "()" in s or "{}" in s or '[]' in s:
+            s = s.replace("()", "").replace('{}', "").replace('[]', "")
+        return s == ''
+        
